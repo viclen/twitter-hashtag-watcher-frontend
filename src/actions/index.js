d37c26f590abfa9dc136e2a1ba608ebb6106ebfd
Victor <victorpbi5@hotmail.com>
2020-07-22 00:03:52 -0400

    Redux

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+export const setList = list => ({
+    type: "SET_LIST",
+    list
+});
+
+export const setApproved = list => ({
+    type: "SET_APPROVED",
+    list
+});
+
+export const setRejected = list => ({
+    type: "SET_REJECTED",
+    list
+});
+
+export const setHashtag = hashtag => ({
+    type: "SET_HASHTAG",
+    hashtag
+});
+
+export const setWatching = watching => ({
+    type: "SET_WATCHING",
+    watching
+});
