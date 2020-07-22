f6560490aca0e536ba345557422817d65226278c
Victor <victorpbi5@hotmail.com>
2020-07-22 00:03:09 -0400

    App index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const store = createStore(reducer)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
